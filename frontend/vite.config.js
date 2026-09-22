a85e42ee171e4a0616311da4b850ffcd4494fc08
Rick <muthokaricky@gmail.com>
2026-09-22 14:28:47 +0300

    Configure GitHub Pages deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/AniTrack/',   
+
   server: {
     proxy: {
       '/api': 'http://localhost:5000'
